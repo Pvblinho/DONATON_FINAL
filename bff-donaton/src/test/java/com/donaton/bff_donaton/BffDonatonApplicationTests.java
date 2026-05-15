2e2aaa184d9ee3d8d8b18a07977c35bad6ef801a
Pvblinho <pabl.salas@duocuc.cl>
2026-05-15 15:35:09 -0400

    se agregan archivos que quedaron pendientes

---
+++ bff-donaton/src/test/java/com/donaton/bff_donaton/BffDonatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donaton.bff_donaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffDonatonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
